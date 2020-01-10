4fff206b95248d0dc3a0901f55ffa2b7b9ff4faf
Professional Student <kintusya@gmail.com>
2020-01-10 19:30:10 +0500

    Full version of code for Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Hi,
 ## Here is my code, made for Programming Assignment #2
 
-## The makeCacheMatrix() function creates a special "matrix" object that can cache its inverse
+## The makeCacheMatrix() function creates a special "matrix" object that can cache its inverse.
 ## det() function tests x. If x is a non-invertible matrix, det() returns an error.
 ## Other functions are almost similar to makeVector() function in the example for this Assesment.
 ## The difference is that we search an inverted matrix instead of mean of a vector
